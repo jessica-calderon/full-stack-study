e55a7c6784a7b569271aed47fc2242f72cf84a0f
jessica-calderon <calderonjessica13@yahoo.com>
2022-08-06 23:18:20 -0500

    route, models, and server restructure

--- server.js
+++ server.js
@@ -1,5 +1,50 @@
 // Dependencies
-const {server, app} = require('./utils/expressHandler');
-const socketHandler = require('./utils/socketHandler');
+// saving this socket logic for last (jc)
+// const {server, app} = require('./utils/expressHandler');
+// const socketHandler = require('./utils/socketHandler');
 
-console.log("Server starting...")
+// console.log("Server starting...")
+
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  // log out after 5 min inactivity
+  //TODO edit max age, make it at least an hour or more....(jc)
+  cookie: { maxAge: 300000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(process.env.PORT || 3001, function () {
+    console.log("Now listening...");
+  });
+});
